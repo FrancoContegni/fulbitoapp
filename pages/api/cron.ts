6af9f4fc84af6e252998ac1626370e274868ee7f
Franco Contegni <contegnifranco@gmail.com>
2023-01-19 13:51:13 -0300

    posible arreglo con start()

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -10,9 +10,12 @@ process.env.QSTASH_NEXT_SIGNING_KEY = "sig_5Xyzim7UERfyEoUzA39feZCDEkko";
 
 mongoose.connect(URI_MONGO).then(() => {
     console.log('Database connected');
+    start()
 }).catch(err => {
     console.error(err);
 });
+
+console.log('Estado de la conexión:', mongoose.connection.readyState);
 
 const teamSchema = new mongoose.Schema({
     league: Number,
@@ -22,40 +25,44 @@ const teamSchema = new mongoose.Schema({
 });
 
 const Team = mongoose.model("Team", teamSchema);
-
-export const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-    try {
-        const result = await axios({
-            method: 'GET',
-            url: 'https://api-football-v1.p.rapidapi.com/v3/teams/statistics',
-            headers: {
-                'X-RapidAPI-Key': '64e1a0803emsh90496dace2b1354p1de268jsn1bb15e616fca',
-                'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-            },
-            params: {
-                league: '39',
-                season: '2020',
-                team: '33'
-            }
-        });
-
-        const team = new Team({ league: 39, season: 2020, team: 33, data: result.data });
-        await team.save();
-
-        res.send("OK");
-    } catch (err) {
-        res.status(500).send(err);
-    } finally {
-        res.end();
+console.log('llega hasta acá?');
+console.log('Estado de la conexión nueva:', mongoose.connection.readyState);
+let handler: any;
+const start = () => {
+    const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+        console.log("If this is printed, the signature has already been verified");
+        try {
+            const result = await axios({
+                method: 'GET',
+                url: 'https://api-football-v1.p.rapidapi.com/v3/teams/statistics',
+                headers: {
+                    'X-RapidAPI-Key': '64e1a0803emsh90496dace2b1354p1de268jsn1bb15e616fca',
+                    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+                },
+                params: {
+                    league: '39',
+                    season: '2020',
+                    team: '33'
+                }
+            });
+    
+            const team = new Team({ league: 39, season: 2020, team: 33, data: result.data });
+            await team.save();
+    
+            res.send("OK");
+    
+        } catch (err) {
+            res.status(500).send(err);
+        } finally {
+            res.end();
+        }
     }
+    console.log('despues de todo');
 }
-
 export default verifySignature(handler);
-
 export const config = {
     api: {
         bodyParser: false,
     },
 };
-
 
