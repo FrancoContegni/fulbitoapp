e566cdd010a6f193308847db937d270b82fce993
Franco Contegni <contegnifranco@gmail.com>
2023-01-25 09:52:44 -0300

    intento ver si funciona el fetch al live

--- pages/api/fixtureLive.ts
+++ pages/api/fixtureLive.ts
@@ -47,13 +47,14 @@ export const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
 
         const fixtures = result.data.response.map(fixture => {
             return new Live({
-                time: fixture.timestamp,
+                time: fixture.fixture.date,
                 status: fixture.status.long,
                 league: fixture.league.name,
                 logo: fixture.league.logo,
                 home: fixture.teams.home.name,
                 away: fixture.teams.away.name,
-                score: fixture.goals,
+                homeGoals: fixture.goals.home,
+                awayGoals: fixture.goals.away,
                 id: fixture.fixture.id
             });
         });
