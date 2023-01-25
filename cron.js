0c5a86ba5d110b321ca84b3b3877f747f7d267a4
Franco Contegni <contegnifranco@gmail.com>
2023-01-25 10:14:17 -0300

    adapto y coloco [0] a response

--- cron.js
+++ cron.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
 const axios = require("axios");
-const verifySignature = require("@upstash/qstash/nextjs").verifySignature;
 
-const ENV_LOCAL_VARIABLE_URI_MONGO = process.env.ENV_LOCAL_VARIABLE_URI_MONGO;
-console.log(ENV_LOCAL_VARIABLE_URI_MONGO);
+const URI_MONGO = "mongodb+srv://fotocopiero:A9xAsXwPH2RDLmlW@cluster0.6tfzzs5.mongodb.net/leagues?retryWrites=true&w=majority"
 
 process.env.QSTASH_CURRENT_SIGNING_KEY = "sig_5uZH86gYxAk8KXVDVkpVsj9KgdJ2";
 process.env.QSTASH_NEXT_SIGNING_KEY = "sig_5Xyzim7UERfyEoUzA39feZCDEkko";
-verifySignature(handler);
 
 
 
-mongoose.connect(process.env.ENV_LOCAL_VARIABLE_URI_MONGO).then(() => {
+mongoose.connect(URI_MONGO).then(() => {
     console.log('Database connected');
 }).catch(err => {
     console.error(err);
@@ -21,28 +18,31 @@ mongoose.connect(process.env.ENV_LOCAL_VARIABLE_URI_MONGO).then(() => {
 
     const options = {
         method: 'GET',
-        url: 'https://api-football-v1.p.rapidapi.com/v3/teams/statistics',
-        params: { league: '39', season: '2020', team: '33' },
+        url: 'https://api-football-v1.p.rapidapi.com/v3/fixtures',
+        params: { id: '923276'},
         headers: {
             'X-RapidAPI-Key': '64e1a0803emsh90496dace2b1354p1de268jsn1bb15e616fca',
             'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
         }
     };
-     console.log('options');
-    const dataSchema = new mongoose.Schema({
-        team: String,
-        form: String,
+
+     const liveSchema = new mongoose.Schema({
+        time: String,
+        status: String,
+        league: String,
         logo: String,
-        name: String
-    
+        home: String,
+        away: String,
+        id: String
     });
-    console.log('dataschema');
-    const DataModel = mongoose.model('england', dataSchema);
 
-console.log('datamodel');
 
-async function handler(_req, res) {
-        console.log("If this is printed, the signature has already been verified");
+const Live = mongoose.model('Live', liveSchema);
+
+
+
+
+
     // Hacer la llamada a la API aquí
     axios.request(options).then(function (response) {
         // Crear un nuevo documento en la base de datos utilizando el modelo de Mongoose
@@ -59,18 +59,9 @@ async function handler(_req, res) {
                 console.log("Documento guardado exitosamente");
             }
         });
-    }).catch(function (error) {
-        console.error(error);
     });
-}
 
 
 
-module.exports = verifySignature(handler);
 
-module.exports.config = {
-  api: {
-    bodyParser: false,
-  },
-};
 
